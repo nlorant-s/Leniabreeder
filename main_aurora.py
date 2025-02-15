fix variance axis

main_aurora.py
@@ -82,7 +82,7 @@ def main(config: DictConfig) -> None:
 	def latent_variance(observation, train_state, key):
 		latents = vae.apply(train_state.params, observation.phenotype[-config.qd.n_keep:], key, method=vae.encode)
 		latent_mean = jnp.mean(latents, axis=-2)
-		return -jnp.mean(jnp.linalg.norm(latents - latent_mean[..., None, :], axis=-1), axis=-2)
+		return -jnp.mean(jnp.linalg.norm(latents - latent_mean[..., None, :], axis=-1), axis=-1)
 
 	def unsupervised(observation, train_state, key):
 		homeostasis = latent_variance(observation, train_state, key)